9ca8cc9ec8d108a8bef5bdd8aa8a02c5f190f248
Ivan Bobev <poondev2x@gmail.com>
2026-02-11 02:49:44 -0500

    Update MainApplication.kt

--- android/app/src/main/java/com/facerecognitionapp/MainApplication.kt
+++ android/app/src/main/java/com/facerecognitionapp/MainApplication.kt
@@ -16,7 +16,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // You need to add packages here to link it with this project.
               // add(MyReactNativePackage())
             }
 
